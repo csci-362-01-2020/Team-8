72ee8ca3d9ef5c82a09b41402680b36eec516919
Grahamet <grahamet@g.cofc.edu>
2020-10-05 22:52:23 -0400

    testCase5

---
+++ TestAutomation/testCasesExecutables/testCase5/testCase5.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class testCase5 {
+
+	public static void main(String[] args) {
+
+		Double argument = Double.parseDouble(args[0]);
+
+		System.out.println("Answer: " + ContrastChecker.getComposantValue(argument));
+
+	}
+}
