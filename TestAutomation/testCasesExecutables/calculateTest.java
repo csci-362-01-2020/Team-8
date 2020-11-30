671c7d9fbcdcf6c951309fc1084eed26ca2a5cc9
Chloe Stapleton <ces@MacBook-Pro.lan>
2020-11-29 19:54:45 -0500

    Add documentation to drivers

--- TestAutomation/testCasesExecutables/calculateTest.java
+++ TestAutomation/testCasesExecutables/calculateTest.java
@@ -1,15 +1,24 @@
+// driver for DistanceCalculator.calculate()
+// return the euclidean distance between two colors
 
 import java.awt.*;
 
 public class calculateTest {
-
-
 	public static void main(String[] args) {
 
 		try {
-			Color color1 = new Color(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-			Color color2 = new Color(Integer.parseInt(args[3]), Integer.parseInt(args[4]), Integer.parseInt(args[5]));
-			System.out.println(DistanceCalculator.calculate(color1,color2));
+			int arg0 = Integer.parseInt(args[0]);
+			int arg1 = Integer.parseInt(args[1]);
+			int arg2 = Integer.parseInt(args[2]);
+			int arg3 = Integer.parseInt(args[3]);
+			int arg4 = Integer.parseInt(args[4]);
+			int arg5 = Integer.parseInt(args[5]);
+
+			Color color1 = new Color(arg0, arg1, arg2);
+			Color color2 = new Color(arg3, arg4, arg5);
+
+			double result = DistanceCalculator.calculate(color1,color2);
+			System.out.println(result);
 		}
 		catch (Exception e){
 			System.out.println("error");
--- TestAutomation/testCasesExecutables/hexrgbTest.java
+++ TestAutomation/testCasesExecutables/hexrgbTest.java
@@ -1,11 +1,14 @@
+// driver for ColorConverter.hex2Rgb()
+// test that the correct RBG is return for a given hex String
+
 import java.awt.*;
 
 public class hexrgbTest {
-
 	public static void main(String[] args) {
 
 		try {
-			System.out.println(ColorConverter.hex2Rgb(args[0]));
+			Color result = ColorConverter.hex2Rgb(args[0]);
+			System.out.println(result);
 		}
 		catch (Exception e){
 			System.out.println("error");
